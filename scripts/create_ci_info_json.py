f43a0ad85bb4b80d5f43a11fad0f7d4b635ab52e
GrzegorzKaczan <kaczan.grzegorz@gmail.com>
2023-07-25 09:21:56 +0200

    test identifier

--- scripts/create_ci_info_json.py
+++ scripts/create_ci_info_json.py
@@ -1,14 +1,20 @@
 import json
 import os
+import uuid
+
+namespace = uuid.NAMESPACE_URL
 
 data = {
-    "repository": os.getenv("GITHUB_REPOSITORY"),
-    "branch": os.getenv("BRANCH_NAME"),
-    "author": os.getenv("GITHUB_ACTOR"),
+    "repositoryName": os.getenv("GITHUB_REPOSITORY"),
+    "branchName": os.getenv("BRANCH_NAME"),
+    "authorName": os.getenv("GITHUB_ACTOR"),
     "prTitle": os.getenv("PR_TITLE"),
     "prNumber": os.getenv("PR_NUMBER"),
     "prUrl": os.getenv("PR_URL"),
 }
+
+# Generate a unique ID for this PR across whole Github
+data["prIdentifier"] = str(uuid.uuid5(namespace, data["repositoryName"] + data["branchName"] + data["prNumber"]))
 
 with open("soda_scan_ci_info.json", "w") as f:
     json.dump(data, f)
